6c4da528f8b569e396f9603cd2a52ef591080f39
bnaman50 <bnaman50@gmail.com>
2020-02-23 19:13:04 -0600

    chnages

---
+++ user_constants.py
@@ -0,0 +1,10 @@
+# Path to dataset 
+DATA_PATH_DICT = {
+    'CIFAR': '/path/tools/cifar',
+    'RestrictedImageNet': '/path/tools/imagenet',
+    'ImageNet': '/home/naman/ImageNet_Val',
+    'H2Z': '/path/tools/horse2zebra',
+    'A2O': '/path/tools/apple2orange',
+    'S2W': '/path/tools/summer2winter_yosemite'
+}  
+
