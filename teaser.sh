16b6b0d6fb0fa3f04fc975ae9dcfb22e96f2c4f6
Chirag <chiragagarwall12@gmail.com>
2020-02-24 01:49:51 -0600

    minor

--- teaser.sh
+++ teaser.sh
@@ -9,12 +9,16 @@
 # Arguments
 l_bp=0
 num_superpixel=50
-l_es=0
+lime_explainer_seed=0
 img_path='./Images/'
 output_path='./results'
 
 # Code
-CUDA_VISIBLE_DEVICES=0 python LIME_Madry.py -idp ${img_path} -op ${output_path} -l_bp ${l_bp} -l_sn ${num_superpixel} -l_es ${l_es}
+for lime_explainer_seed in 1 2 3 
+do
+    CUDA_VISIBLE_DEVICES=0 python LIME_Madry.py -idp ${img_path} -op ${output_path} -l_bp ${l_bp} -l_sn ${num_superpixel} -l_es ${lime_explainer_seed}
+done
+
 
 ## Meaningful Perturbation ##
 # Arguments
@@ -27,7 +31,10 @@ save_npy=1
 num_iter=300
 
 # Code
-CUDA_VISIBLE_DEVICES=0 python MP_MADRY.py --seed ${num_seed} --img_dir_path ${img_path} --out_path ${output_path} --mask_init ${init_mask} --mask_init_size ${mask_size} --if_save_mask_evolution ${save_evolution_mask} --if_save_plot ${save_plot} --if_save_npy ${save_npy} --num_iter ${num_iter}
+for blur_radius in 5 10 30
+do
+    CUDA_VISIBLE_DEVICES=0 python MP_MADRY.py --seed ${num_seed} --img_dir_path ${img_path} --out_path ${output_path} --mask_init ${init_mask} --mask_init_size ${mask_size} --if_save_mask_evolution ${save_evolution_mask} --if_save_plot ${save_plot} --if_save_npy ${save_npy} --num_iter ${num_iter} --blur_radius ${blur_radius}
+done
 
 
 ## Smooth Gradient ##
@@ -38,7 +45,11 @@ num_samples=50
 std_dev=0.3
 
 # Code
-CUDA_VISIBLE_DEVICES=0 python SmoothGrad_Madry.py -idp ${img_path}  -if_n ${add_noise} --idx_flag ${index_flag} -op ${output_path} -n_sam ${num_samples} -std ${std_dev}
+for num_samples in 50 200 800
+do
+    CUDA_VISIBLE_DEVICES=0 python SmoothGrad_Madry.py -idp ${img_path}  -if_n ${add_noise} --idx_flag ${index_flag} -op ${output_path} -n_sam ${num_samples} -std ${std_dev}
+done
+
 
 ## Sliding-Patch ##
 # Arguments
@@ -46,4 +57,7 @@ num_seed=0
 patch_size=53
 
 # Code
-python Occlusion_Madry.py -np_s ${num_seed} -idp ${img_path}  -op ${output_path}  -ops ${patch_size}
+for patch_size in 5 29 53
+do
+    CUDA_VISIBLE_DEVICES=0 python Occlusion_Madry.py -np_s ${num_seed} -idp ${img_path}  -op ${output_path}  -ops ${patch_size}
+done
