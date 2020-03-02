82d0bb68e0fddcec5451068e08ab7649bbf8a467
Chirag <chiragagarwall12@gmail.com>
2020-03-02 15:59:06 -0600

    adversarial training

---
+++ train.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#
+# Chirag Agarwal <chiragagarwall12.gmail.com>
+# Naman Bansal <bnaman50@gmail.com>
+# 2020
+
+# Adversarial training script for GoogLeNet-R
+data_path='/mnt/raid/chirag/ILSVRC2012/'
+output_dir='./imagenet_googlenet/'
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m robustness.main --dataset imagenet --data ${data_path} --adv-train 1 --arch googlenet --out-dir ${output_dir} --epochs 90 --lr 0.1 --batch-size 512 --step-lr 30 --eps 3 --attack-lr 0.5 --attack-steps 7 --constraint 2
