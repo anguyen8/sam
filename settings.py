5c9c1baf3c59424f909ca3448b519426eb1f87cf
bnaman50 <bnaman50@gmail.com>
2020-03-05 16:52:16 -0600

    minor

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-imagenet_val_path = '~/CS231n/heatmap_tests/images/ILSVRC2012_img_val/' # change it as per your usage
-imagenet_val_xml_path = '~/CS231n/heatmap_tests/images/ILSVRC2012_img_val_bb_xml/' # change it as per your usage
-paper_img_txt_file = "/home/naman/CS231n/heatmap_tests/Madri/Madri_New/sam/img_name_files/paper_images_list.txt" # change it as per your usage
-imagenet_class_mappings = '/home/naman/CS231n/heatmap_tests/Madri/Madri_New/sam/imagenet_class_mappings' # change it as per your usage
+imagenet_train_path='/mnt/raid/chirag/ILSVRC2012/' # change it as per your usage
+imagenet_val_path='~/CS231n/heatmap_tests/images/ILSVRC2012_img_val/' # change it as per your usage
+imagenet_val_xml_path='~/CS231n/heatmap_tests/images/ILSVRC2012_img_val_bb_xml/' # change it as per your usage
+paper_img_txt_file="/home/naman/CS231n/heatmap_tests/Madri/Madri_New/sam/img_name_files/paper_images_list.txt" # change it as per your usage
+imagenet_class_mappings='/home/naman/CS231n/heatmap_tests/Madri/Madri_New/sam/imagenet_class_mappings' # change it as per your usage
--- train.sh
+++ train.sh
@@ -5,7 +5,8 @@
 # 2020
 
 # Adversarial training script for GoogLeNet-R
-data_path='/mnt/raid/chirag/ILSVRC2012/'
+source settings.py
+data_path=$imagenet_train_path
 output_dir='./imagenet_googlenet/'
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m robustness.main --dataset imagenet --data ${data_path} --adv-train 1 --arch googlenet --out-dir ${output_dir} --epochs 90 --lr 0.1 --batch-size 512 --step-lr 30 --eps 3 --attack-lr 0.5 --attack-steps 7 --constraint 2
